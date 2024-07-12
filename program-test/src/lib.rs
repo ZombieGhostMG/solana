54f656dad96f6baf3bbe24048b1bebdbd5bbc052
ZombieGhostMG <zombieghostmg@gmail.com>
2024-07-12 18:44:05 +0100

    Revert program file path changes

--- program-test/src/lib.rs
+++ program-test/src/lib.rs
@@ -623,13 +623,13 @@ impl ProgramTest {
         program_name: &'static str,
         program_id: &Pubkey,
     ) {
-        let add_bpf = |this: &mut ProgramTest, program_file: PathBuf| {
-            let elf = read_file(&program_file);
-            let program_accounts =
-                programs::bpf_loader_upgradeable_program_accounts(program_id, &elf, &Rent::default());
-            for (address, account) in program_accounts {
-                self.add_genesis_account(address, account);
-            }
+        let program_file = find_file(&format!("{program_name}.so"))
+            .expect("Program file data not available for {program_name} ({program_id})");
+        let elf = read_file(program_file);
+        let program_accounts =
+            programs::bpf_loader_upgradeable_program_accounts(program_id, &elf, &Rent::default());
+        for (address, account) in program_accounts {
+            self.add_genesis_account(address, account);
         };
     }
 
